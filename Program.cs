f39099a2b9c19ab908ef312670c0fec58f579c87
AndreiParmon <parmonandrei@gmail.com>
2023-01-24 20:06:03 +0300

    add task FinalTestWork

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string[] array8 = new string[] {"1234", "1567", "-2", "computer science", "/*-+", "a15"};
+string[] result = new string[array8.Length];
+
+void ArrayMas(string[] array8, string[] result)
+{
+    int count = 0;
+    for (int i = 0; i < array8.Length; i++)
+    {
+        if (array8[i].Length <= 3)
+        {   
+            result[count] = array8[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+ArrayMas(array8, result);
+Console.Write("Сформированный массив: ");
+PrintArray(result);
